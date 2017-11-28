be78585e0fbfa11be078a91dc7dff519e78dd593
TacticalLuke43 <luke.jeries@gmail.com>
2017-11-28 15:55:29 -0500

    Rearranged some files
    Updates to Log In screen and Main Menu

--- app/src/main/java/csc483/scoremaster/LogInScreen.java
+++ app/src/main/java/csc483/scoremaster/LogInScreen.java
@@ -1,9 +1,24 @@
 package csc483.scoremaster;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LogInScreen extends AppCompatActivity {
+public class LogInScreen extends AppCompatActivity{
+
+    private EditText txtEmail;
+    private EditText txtPass;
+
+    private Button btnLogin;
+    private Button btnRegister;
+
+    private ProgressDialog mProgress;
+
+    //firebase
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
