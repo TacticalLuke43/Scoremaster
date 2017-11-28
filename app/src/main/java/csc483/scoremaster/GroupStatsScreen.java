ed69044720f237c948a38c5dcf0feb5c9e03c20d
tashepha <tashepha@umflint.edu>
2017-11-28 17:21:45 -0500

    Add files via upload

--- app/src/main/java/csc483/scoremaster/GroupStatsScreen.java
+++ app/src/main/java/csc483/scoremaster/GroupStatsScreen.java
@@ -1,13 +1,85 @@
 package csc483.scoremaster;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
+import android.support.v4.media.RatingCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+//import GroupStats class
 
 public class GroupStatsScreen extends AppCompatActivity {
+
+
+    TableLayout groupTable = (TableLayout)findViewById(R.id.groupStatTable);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_stats_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // replace placeholder 2 with getnumplayers() to get the number of players in the group
+        int numberOfPlayers = 2;
+
+        //loops through the players and dynamically creates new table rows for them
+        //will likely need to change this depending on how we are storing data/what the group class looks like
+        for (int i=0; i<numberOfPlayers; i++)
+        {
+            TableRow rowThing = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            rowThing.setLayoutParams(lp);
+
+            TextView playername = new TextView(this);
+            playername.setLayoutParams(new TableLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+            playername.setText("Thing");//change to getPlayerName() when class is done
+            playername.setBackground(Drawable.createFromPath("@android:drawable/alert_light_frame"));
+            playername.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+
+            TextView avgInning = new TextView(this);
+            avgInning.setLayoutParams(new TableLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+            avgInning.setText("0");//change to getAvgInning() when class is done
+            avgInning.setBackground(Drawable.createFromPath("@android:drawable/alert_light_frame"));
+            avgInning.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+            avgInning.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
+            TextView matchWin = new TextView(this);
+            matchWin.setLayoutParams(new TableLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+            matchWin.setText("0");//change to getMatchWins() when class is done
+            matchWin.setBackground(Drawable.createFromPath("@android:drawable/alert_light_frame"));
+            matchWin.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+            matchWin.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
+            TextView matchPlayed = new TextView(this);
+            matchPlayed.setLayoutParams(new TableLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+            matchPlayed.setText("0");//change to getMatchesPlayed() when class is done
+            matchPlayed.setBackground(Drawable.createFromPath("@android:drawable/alert_light_frame"));
+            matchPlayed.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+            matchPlayed.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
+            TextView winPercent = new TextView(this);
+            winPercent.setLayoutParams(new TableLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+            winPercent.setText("0%");//change to getWinPercentage() when class is done
+            winPercent.setBackground(Drawable.createFromPath("@android:drawable/alert_light_frame"));
+            winPercent.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+            winPercent.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
+            rowThing.addView(playername);
+            rowThing.addView(avgInning);
+            rowThing.addView(matchWin);
+            rowThing.addView(matchPlayed);
+            rowThing.addView(winPercent);
+            groupTable.addView(rowThing, i);
+
+        }
+
     }
 }
--- app/src/main/java/csc483/scoremaster/PlayerStatsScreen.java
+++ app/src/main/java/csc483/scoremaster/PlayerStatsScreen.java
@@ -2,12 +2,48 @@ package csc483.scoremaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+//Import PlayerStats class
 
 public class PlayerStatsScreen extends AppCompatActivity {
+
+    //Text View Stuff
+    TextView Break = (TextView)findViewById(R.id.OnBreak);
+    TextView Run = (TextView)findViewById(R.id.BallAndRun);
+    TextView Outofturn = (TextView)findViewById(R.id.OutOfTurn);
+    TextView Scratch = (TextView)findViewById(R.id.ScratchOn8);
+    TextView Wins = (TextView)findViewById(R.id.MatchWins);
+    TextView Played = (TextView)findViewById(R.id.MatchPlayed);
+    TextView WinPercent = (TextView)findViewById(R.id.WinPercent);
+    TextView Innings = (TextView)findViewById(R.id.AvgInnings);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_stats_screen);
+
+        //Getting stats info and placing into strings
+        //commented out until database works
+        //R.string.PlayerStats_Break = String.valueOf(getBreaks());
+        //R.string.PlayerStats_Run = String.valueOf(getRuns());
+        //R.string.PlayerStats_Outofturn = String.valueOf(getOutofturn());
+        //R.string.PlayerStats_Scratch = String.valueOf(getScratchOn8());
+        //R.string.PlayerStats_Wins = String.valueOf(getMatchesWon());
+        //R.string.PlayerStats_Played = String.valueOf(getMatchesPlayed());
+        //R.string.PlayerStats_WinPercent = String.format(calcWinPercent(), percent);
+        //R.string.PlayerStats_Innings = String.valueOf(calcAverageInnings());
+
+        //Setting the TextViews
+        Break.setText(R.string.PlayerStats_Break);
+        Run.setText(R.string.PlayerStats_Run);
+        Outofturn.setText(R.string.PlayerStats_Outofturn);
+        Scratch.setText(R.string.PlayerStats_Scratch);
+        Wins.setText(R.string.PlayerStats_Wins);
+        Played.setText(R.string.PlayerStats_Played);
+        WinPercent.setText(R.string.PlayerStats_WinPercent);
+        Innings.setText(R.string.PlayerStats_Innings);
+
     }
 }
