f75e779c911b30dc020dfd4d6697da7f55425052
TacticalLuke43 <luke.jeries@gmail.com>
2017-12-09 16:48:01 -0500

    Orientation fix
    fixed the orientation in score screen

--- app/src/main/java/csc483/scoremaster/ScoringScreen.java
+++ app/src/main/java/csc483/scoremaster/ScoringScreen.java
@@ -1,5 +1,6 @@
 package csc483.scoremaster;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class ScoringScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
