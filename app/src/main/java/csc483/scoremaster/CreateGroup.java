37fc74b79ce0e37c986b2ceb6476785eb2cabfc4
TacticalLuke43 <luke.jeries@gmail.com>
2017-12-10 15:58:33 -0500

    Join.Create Group Firebase access

--- app/src/main/java/csc483/scoremaster/CreateGroup.java
+++ app/src/main/java/csc483/scoremaster/CreateGroup.java
@@ -7,11 +7,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CreateGroup extends AppCompatActivity {
-
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,29 +25,18 @@ public class CreateGroup extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String groupName = ((EditText)findViewById(R.id.groupName)).getText().toString();
-
-                //if groupName is not taken
-                if (!Objects.equals(groupName, "something"))//needs to be getGroupList()
-                {
-                    //randomly generate group code
-                    RandomGen gen = new RandomGen(8, ThreadLocalRandom.current());
-                    //gen = group code
-                    //create group
-
-                    //add player to group
-
-                    // give player admin/edit permissions
-
-                    finish();
-                }
-                else
-                {
-                    //  retry
-                    Toast.makeText(CreateGroup.this,
-                            "Name Already taken or is invalid, try again", Toast.LENGTH_LONG).show();
-                }
-
-
+                //randomly generate group code
+                RandomGen gen = new RandomGen(6, ThreadLocalRandom.current());
+                String groupID = gen.nextString();
+                //gen = group code
+                //create group
+                myRef = FirebaseDatabase.getInstance().getReference().child("groups").child(groupID);
+                myRef.child("name").setValue(groupName);
+                myRef.child("playerList").child("player1").setValue("AAA111");
+                //add player to group
+                myRef = FirebaseDatabase.getInstance().getReference().child("players").child("AAA111");
+                myRef.child("groupList").child("3").setValue(groupID);
+                finish();
             }
         });
     }
