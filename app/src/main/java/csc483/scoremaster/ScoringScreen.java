d5df374a974df8ed511cb2c2af98f792b644bccc
TacticalLuke43 <luke.jeries@gmail.com>
2017-12-09 21:51:10 -0500

    Game Saving
    scoring screen now saves game data after each game

--- app/src/main/java/csc483/scoremaster/ScoringScreen.java
+++ app/src/main/java/csc483/scoremaster/ScoringScreen.java
@@ -7,15 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ScoringScreen extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user;
+
+    String roomCode = "temp";
     int gameCount = 1;
     TextView textView27;
     TextView textView38;
     TextView textView39;
     TextView textView40;
+    TextView matchCodeText;
     CheckBox checkBox3;
     CheckBox checkBox4;
     CheckBox checkBox5;
@@ -34,6 +50,20 @@ public class ScoringScreen extends AppCompatActivity {
     CheckBox checkBox18;
     RadioGroup radioGroup2;
     RadioGroup radioGroup3;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+    RadioButton radioButton4;
+    RadioButton radioButton5;
+    RadioButton radioButton6;
+    CheckBox player18OB;
+    CheckBox player28OB;
+    CheckBox player18BR;
+    CheckBox player28BR;
+    CheckBox player18OT;
+    CheckBox player28OT;
+    CheckBox player1SO8;
+    CheckBox player2SO8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,15 +76,37 @@ public class ScoringScreen extends AppCompatActivity {
         finishBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(gameCount == 3)
+                {
+                    myRef = database.getInstance().getReference().child("matches");
+                    myRef.child(roomCode).child("game3").child("player18OB").setValue(checkBox11.isChecked());
+                    myRef.child(roomCode).child("game3").child("player28OB").setValue(checkBox12.isChecked());
+                    myRef.child(roomCode).child("game3").child("player18BR").setValue(checkBox13.isChecked());
+                    myRef.child(roomCode).child("game3").child("player28BR").setValue(checkBox14.isChecked());
+                    myRef.child(roomCode).child("game3").child("player18OT").setValue(checkBox15.isChecked());
+                    myRef.child(roomCode).child("game3").child("player28OT").setValue(checkBox16.isChecked());
+                    myRef.child(roomCode).child("game3").child("player1SO8").setValue(checkBox17.isChecked());
+                    myRef.child(roomCode).child("game3").child("player2SO8").setValue(checkBox18.isChecked());
+                    myRef.child(roomCode).child("game3").child("player18win").setValue(radioButton5.isChecked());
+                    myRef.child(roomCode).child("game3").child("player28win").setValue(radioButton6.isChecked());
+                }
                 startActivity(new Intent(ScoringScreen.this, PostGameScreen.class));
             }
         });
-
         Button nextBut = (Button)findViewById(R.id.nextGameButton);
+        matchCodeText = (TextView)findViewById(R.id.matchCodeText);
         textView27 = (TextView)findViewById(R.id.textView27);
         textView38 = (TextView)findViewById(R.id.textView38);
         textView39 = (TextView)findViewById(R.id.textView39);
         textView40 = (TextView)findViewById(R.id.textView40);
+        player18OB  = (CheckBox)findViewById(R.id.player18OB);
+        player28OB  = (CheckBox)findViewById(R.id.player28OB);
+        player18BR  = (CheckBox)findViewById(R.id.player18BR);
+        player28BR  = (CheckBox)findViewById(R.id.player28BR);
+        player18OT  = (CheckBox)findViewById(R.id.player18OT);
+        player28OT  = (CheckBox)findViewById(R.id.player28OT);
+        player1SO8  = (CheckBox)findViewById(R.id.player1SO8);
+        player2SO8  = (CheckBox)findViewById(R.id.player2SO8);
         checkBox3 = (CheckBox)findViewById(R.id.checkBox3);
         checkBox4 = (CheckBox)findViewById(R.id.checkBox4);
         checkBox5 = (CheckBox)findViewById(R.id.checkBox5);
@@ -73,6 +125,24 @@ public class ScoringScreen extends AppCompatActivity {
         checkBox18 = (CheckBox)findViewById(R.id.checkBox18);
         radioGroup2 = (RadioGroup)findViewById(R.id.radioGroup2);
         radioGroup3 = (RadioGroup)findViewById(R.id.radioGroup3);
+        radioButton1 = (RadioButton)findViewById(R.id.radioPlayer1);
+        radioButton2 = (RadioButton)findViewById(R.id.radioPlayer2);
+        radioButton3 = (RadioButton)findViewById(R.id.radioButton);
+        radioButton4 = (RadioButton)findViewById(R.id.radioButton2);
+        radioButton5 = (RadioButton)findViewById(R.id.radioButton3);
+        radioButton6 = (RadioButton)findViewById(R.id.radioButton4);
+
+
+        RandomGen gen = new RandomGen(6, ThreadLocalRandom.current());
+        roomCode = gen.nextString();
+        matchCodeText.setText(roomCode);
+        myRef = database.getInstance().getReference().child("matches");
+        myRef.child(roomCode).child("player1").setValue("AAA111");
+        myRef.child(roomCode).child("player2").setValue("AAA112");
+        myRef.child(roomCode).child("group").setValue("ABC123");
+        myRef.child(roomCode).child("innings").setValue(8);
+
+
         nextBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,6 +159,17 @@ public class ScoringScreen extends AppCompatActivity {
                     checkBox9.setVisibility(View.VISIBLE);
                     checkBox10.setVisibility(View.VISIBLE);
                     radioGroup2.setVisibility(View.VISIBLE);
+                    myRef = database.getInstance().getReference().child("matches");
+                    myRef.child(roomCode).child("game1").child("player18OB").setValue(player18OB.isChecked());
+                    myRef.child(roomCode).child("game1").child("player28OB").setValue(player28OB.isChecked());
+                    myRef.child(roomCode).child("game1").child("player18BR").setValue(player18BR.isChecked());
+                    myRef.child(roomCode).child("game1").child("player28BR").setValue(player28BR.isChecked());
+                    myRef.child(roomCode).child("game1").child("player18OT").setValue(player18OT.isChecked());
+                    myRef.child(roomCode).child("game1").child("player28OT").setValue(player28OT.isChecked());
+                    myRef.child(roomCode).child("game1").child("player1SO8").setValue(player1SO8.isChecked());
+                    myRef.child(roomCode).child("game1").child("player2SO8").setValue(player2SO8.isChecked());
+                    myRef.child(roomCode).child("game1").child("player18win").setValue(radioButton1.isChecked());
+                    myRef.child(roomCode).child("game1").child("player28win").setValue(radioButton2.isChecked());
                     gameCount++;
                 }
                 else if(gameCount == 2)
@@ -104,6 +185,17 @@ public class ScoringScreen extends AppCompatActivity {
                     checkBox17.setVisibility(View.VISIBLE);
                     checkBox18.setVisibility(View.VISIBLE);
                     radioGroup3.setVisibility(View.VISIBLE);
+                    myRef = database.getInstance().getReference().child("matches");
+                    myRef.child(roomCode).child("game2").child("player18OB").setValue(checkBox3.isChecked());
+                    myRef.child(roomCode).child("game2").child("player28OB").setValue(checkBox4.isChecked());
+                    myRef.child(roomCode).child("game2").child("player18BR").setValue(checkBox5.isChecked());
+                    myRef.child(roomCode).child("game2").child("player28BR").setValue(checkBox6.isChecked());
+                    myRef.child(roomCode).child("game2").child("player18OT").setValue(checkBox7.isChecked());
+                    myRef.child(roomCode).child("game2").child("player28OT").setValue(checkBox8.isChecked());
+                    myRef.child(roomCode).child("game2").child("player1SO8").setValue(checkBox9.isChecked());
+                    myRef.child(roomCode).child("game2").child("player2SO8").setValue(checkBox10.isChecked());
+                    myRef.child(roomCode).child("game2").child("player18win").setValue(radioButton3.isChecked());
+                    myRef.child(roomCode).child("game2").child("player28win").setValue(radioButton4.isChecked());
                     gameCount++;
                 }
             }
